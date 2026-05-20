66d1ee9d0a78f61ab09d9fcafb1aadfbdc17fbe8
Vasu Kolli <vkolli@rosen-group.com>
2026-05-20 08:07:30 +0200

    branch changed to unstable due to devenv

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs = {
     # The DPI-bypassing nixpkgs URL that worked
-    nixpkgs.url = "https://channels.nixos.org/nixos-25.11/nixexprs.tar.xz";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
 
     # Home Manager
     home-manager = {
-      url = "git+https://github.com/nix-community/home-manager.git?ref=release-25.11"; 
+      url = "git+https://github.com/nix-community/home-manager.git?ref=master"; 
       inputs.nixpkgs.follows = "nixpkgs"; 
     };
     
