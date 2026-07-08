9996e9b5c253b3d4df66430605a40623673defd5
Vasu Kolli <vkolli@rosen-group.com>
2026-07-08 13:13:16 +0200

    substituters added

--- home.nix
+++ home.nix
@@ -36,6 +36,18 @@
       dates = "daily";
       options = "--delete-older-than +3";
     };
+    
+    # substituters
+    settings = {
+      extra-substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
